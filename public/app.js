06260e930b1c4074b07fded4b7b77bc8b69565de
Robert Henderson <robert@nterpryz.com>
2018-02-20 14:31:18 +0000

    working app

--- public/app.js
+++ public/app.js
@@ -9,6 +9,9 @@ const init = function () {
 const populate = function (todos) {
   // this function needs to:
   // - loop through the array of todos, invoking addItem() for each todo item
+  for(const todo of todos){
+    addItem(todo)
+  }
 }
 
 const addItem = function (item) {
@@ -16,6 +19,9 @@ const addItem = function (item) {
   // this function needs to:
   // - create an li element containing the string 'item'
   // - append the li to the "todo-list" ul
+  const listItem = document.createElement('li')
+  listItem.innerText = item;
+  ul.appendChild(listItem);
 }
 
 const handleButtonClick = function () {
@@ -23,6 +29,11 @@ const handleButtonClick = function () {
   // - get hold of the input box's value
   // - append it to the "todo-list" ul by invoking addItem()
   // - add it to local storage by invoking save()
+  const input = document.querySelector('#new-item')
+  const newItem = input.value;
+  input.value = '';
+  addItem(newItem);
+  save(newItem);
 }
 
 const save = function (newItem) {
@@ -31,6 +42,11 @@ const save = function (newItem) {
   // - add the newItem to the array
   // - stringify the updated array
   // - save it back to localstoage
+  const oldList = localStorage.getItem('todoList') || "[]"
+  const savedArray = JSON.parse(oldList)
+  savedArray.push(newItem)
+  const newList = JSON.stringify(savedArray);
+  localStorage.setItem('todoList', newList);
 }
 
-document.addEventListener("DOMContentLoaded", app);
+document.addEventListener("DOMContentLoaded", init);
